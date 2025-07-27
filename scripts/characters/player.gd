e845b72e7357adbe5d9fedecdb097ba3e4659b26
LynnePlex <38146775+BliniMango@users.noreply.github.com>
2025-07-27 16:43:09 -0500

    0.0.2 Add player run and stand animations

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@onready var animation_player_upper = $AnimationPlayerUpper
+@onready var animation_player_lower = $AnimationPlayerLower
+
 
 func _physics_process(delta: float) -> void:
 	var x_input = Input.get_axis("move_left", "move_right")
@@ -7,5 +10,12 @@ func _physics_process(delta: float) -> void:
 	
 	velocity.x = x_input * 50
 	velocity.y = y_input * 50
+	
+	if x_input == 0:
+		animation_player_lower.play("stand")
+		animation_player_upper.play("stand")
+	else:
+		animation_player_lower.play("run")
+		animation_player_upper.play("run")
 	
 	move_and_slide()
